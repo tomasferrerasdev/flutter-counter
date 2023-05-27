d42a3affa6f40bbada8246e24819e1463622a73c
Tomas Ferreras <hellotomasdev@gmail.com>
2023-05-27 18:17:43 -0300

    first commit

---
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class CounterFunctionsScreen extends StatefulWidget {
+  const CounterFunctionsScreen({super.key});
+
+  @override
+  State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
+}
+
+class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter Functions'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$clickCounter',
+                style:
+                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              ),
+              Text('Click${clickCounter == 1 ? '' : 's'}',
+                  style: const TextStyle(fontSize: 25))
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  clickCounter++;
+                });
+              },
+              child: const Icon(Icons.plus_one),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  clickCounter--;
+                });
+              },
+              child: const Icon(Icons.exposure_minus_1_outlined),
+            ),
+          ],
+        ));
+  }
+}
